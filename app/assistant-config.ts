dd00319f1830e9f912ad06e7596c6092ffed1527
iessel-git <isaac@csetechsolution.com>
2025-07-19 21:13:12 -0400

    Update assistant-config.ts

--- app/assistant-config.ts
+++ app/assistant-config.ts
@@ -1,4 +1,4 @@
-export let assistantId = ""; // set your assistant ID here
+export let assistantId = "asst_8wXL4lFPmEJ8zxMW1Tsz9ePz"; // set your assistant ID here
 
 if (assistantId === "") {
   assistantId = process.env.OPENAI_ASSISTANT_ID;
